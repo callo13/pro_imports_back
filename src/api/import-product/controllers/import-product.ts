480428e095755a123be1c10953358e1e5ae07cb5
Flo <flasback9@gmail.com>
2025-09-04 11:14:56 +0200

    Init

---
+++ src/api/import-product/controllers/import-product.ts
@@ -0,0 +1,7 @@
+/**
+ * import-product controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::import-product.import-product');
---
+++ src/api/import-product/services/import-product.ts
@@ -0,0 +1,7 @@
+/**
+ * import-product service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::import-product.import-product');
