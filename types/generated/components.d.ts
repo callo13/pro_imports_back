2fe3c2d8a42686ea11efcf445d9861158b3e15fc
Flo <flasback9@gmail.com>
2025-09-20 14:48:58 +0200

    Pastille add

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,22 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface PastillePastille extends Struct.ComponentSchema {
+  collectionName: 'components_pastille_pastilles';
+  info: {
+    displayName: 'Pastille';
+  };
+  attributes: {
+    color1: Schema.Attribute.String &
+      Schema.Attribute.CustomField<'plugin::color-picker.color'>;
+    color2: Schema.Attribute.String &
+      Schema.Attribute.CustomField<'plugin::color-picker.color'>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'pastille.pastille': PastillePastille;
+    }
+  }
+}
